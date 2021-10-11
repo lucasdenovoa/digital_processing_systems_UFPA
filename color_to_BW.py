78e938750f4c0a1249af8c1c928f1b262d884bee
lucasdenovoa <lucasnmartins300@gmail.com>
2021-10-11 16:59:46 -0300

    Update...

--- color_to_BW.py
+++ color_to_BW.py
@@ -2,28 +2,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
+import sys
+import argparse
+
 
 from PIL import Image
 
-img = Image.open('./4.2.07.tiff')
-plt.figure(figsize=(9, 6))
-plt.imshow(img)
+parser = argparse.ArgumentParser(description="Convert color image into B&W")
 
-imggray = img.convert('LA')
+parser.add_argument(
+            "-i",
+            "--image",
+            type=str,
+            help="run with pepper splash and baboon argumments",
+)
 
-imggray.save('converted_4.2.07.tiff')
+args = parser.parse_args()
 
-plt.figure(figsize=(9,6))
-plt.imshow(imggray)
+if args.images:
+    if args.images == "pepper":
+        gray_image = './images/gray_4.2.07.tiff'
+        color_image = './images/4.2.07.tiff'
+    elif args.images == "splash":
+        gray_image = './images/gray_4.2.01.tiff'
+        color_image = './images/4.2.01.tiff'
+    elif args.images == "baboon":
+        gray_image = './images/gray_4.2.03.tiff'
+        color_image = './images/4.2.03.tiff'
+        
+    else:
+        print("invalid argumment: ", args.images)
+        print("Use pepper splash and baboon argumments")
 
-imgmat = np.array(list(imggray.getdata(band=0)), float)
+def color_to_bw():
+    img = Image.open(color_image)
+    plt.figure(figsize=(9, 6))
+    plt.imshow(img)
+    
+    imggray = img.convert('LA')
 
-imgmat.shape = (imggray.size[1], imggray.size[0])
+    imggray.save(gray_image)
 
-imgmat = np.matrix(imgmat)
+def main():
+    color_to_bw()
 
-plt.figure(figsize=(9,6))
-plt.imshow(imgmat, cmap='gray')
-
-#imgmat.save('mat.tiff')
+if __name__ == "__main__":
+    main()
 
--- compression_PSNR.py
+++ compression_PSNR.py
@@ -1,21 +1,67 @@
 from math import log10, sqrt
 import cv2
 import numpy as np
-  
+import matplotlib.pyplot as plt
+from PIL import Image
+
+import os
+
+MAX_PIXEL=255.0
+
+'''
+def read_image():
+    y = Image.open('./images/4.2.07.tiff')
+
+    if os.path.exists('./images/gray_4.2.07.tiff'):
+        pepper_image = Image.open('./images/4.2.07.tiff')
+    elif os.path.exists('./images/gray_4.2.01.tiff'):
+        splash_image = Image.open('./images/4.2.01.tiff')
+    elif os.path.exists('./images/gray_4.2.03.tiff'):
+        baboon_image = Image.open('./images/4.2.03.tiff')
+    
+    
+    plt.figure(2)
+    plt.imshow(y, 'gray', clim=(0, 255))
+
+    y2 = 2*np.floor_divide(y,2)
+    
+    plt.figure(3), plt.imshow(y2, 'gray', clim=(0, 255))
+
+    plt.show()
+'''
+
 def PSNR(original, compressed):
     mse = np.mean((original - compressed) ** 2)
-    if(mse == 0):  # MSE is zero means no noise is present in the signal .
-                  # Therefore PSNR have no importance.
+    if(mse == 0):
         return 100
-    max_pixel = 255.0
-    psnr = 20 * log10(max_pixel / sqrt(mse))
+
+    psnr = 20 * log10(MAX_PIXEL / sqrt(mse))
+    
     return psnr
   
 def main():
-     original = cv2.imread("4.2.07.tiff")
-     compressed = cv2.imread("converted_4.2.07.tiff", 1)
-     value = PSNR(original, compressed)
-     print(f"PSNR value is {value} dB")
+    pepper_original = cv2.imread("./images/4.2.07.tiff")
+
+    if os.path.exists('./images/gray_4.2.07.tiff'):
+        pepper_original = cv2.imread('./images/4.2.07.tiff')
+        pepper_compressed = cv2.imread('./images/gray_4.2.07.tiff')
+        
+        value = PSNR(pepper_original, pepper_compressed)
+        print(f"PSNR value for Pepper image is {value} dB")
+
+    if os.path.exists('./images/gray_4.2.01.tiff'):
+        splash_original = cv2.imread('./images/4.2.01.tiff')
+        splash_compressed = cv2.imread('./images/gray_4.2.01.tiff')
+
+        value = PSNR(splash_original, splash_compressed)
+        print(f"PSNR value for Splash image is {value} dB")
+
+    if os.path.exists('./images/gray_4.2.03.tiff'):
+        baboon_original = cv2.imread('./images/4.2.03.tiff')
+        baboon_compressed = cv2.imread('./images/gray_4.2.03.tiff')
+
+        value = PSNR(baboon_original, baboon_compressed)
+        print(f"PSNR value for Baboon image is {value} dB")
        
 if __name__ == "__main__":
     main()
